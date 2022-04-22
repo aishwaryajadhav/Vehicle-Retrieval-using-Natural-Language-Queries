fcf4ff250b5175662d7c81248059e8d5c1da2290
Vivek Sourabh <viveksourabh2808@gmail.com>
2022-04-22 11:18:51 -0400

    add VideoDataset loader

--- AIC2021-T5-CLV-main/datasets.py
+++ AIC2021-T5-CLV-main/datasets.py
@@ -1,13 +1,19 @@
+'''
+Video Dataset code reused from -> https://github.com/moabitcoin/ig65m-pytorch/blob/master/ig65m/datasets.py
+'''
 import json
 import os
 import random
 from PIL import Image
 import cv2
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, get_worker_info
 import torch.nn.functional as F
 import torchvision
 from utils import get_logger
+import math
+
+
 def default_loader(path):
     return Image.open(path).convert('RGB')
 
@@ -98,8 +104,6 @@ class CityFlowNLDataset(Dataset):
 
 
 
-
-
 class CityFlowNLInferenceDataset(Dataset):
     def __init__(self, data_cfg,transform = None):
         """Dataset for evaluation. Loading tracks instead of frames."""
@@ -143,3 +147,113 @@ class CityFlowNLInferenceDataset(Dataset):
             return crop,bk,track["track_id"],track["frames_id"]
         return crop,track["track_id"],track["frames_id"]
 
+
+class CityFlowNLVideoDataset(Dataset):
+    def __init__(self, data_cfg,json_path,transform = None,Random= True):
+        """
+        Dataset to use video data for training.
+        :param data_cfg: CfgNode for CityFlow NL.
+        """
+        self.data_cfg = data_cfg.clone()
+        self.crop_area = data_cfg.CROP_AREA
+        self.random = Random
+        with open(json_path) as f:
+            tracks = json.load(f)
+        self.list_of_uuids = list(tracks.keys())
+        self.list_of_tracks = list(tracks.values())
+        self.transform = transform
+        self.bk_dic = {}
+        self._logger = get_logger()
+        
+        self.all_indexs = list(range(len(self.list_of_uuids)))
+        print(len(self.all_indexs))
+        print("data load")
+
+    def __len__(self):
+        return len(self.all_indexs)
+
+    def __getitem__(self, index):   
+        tmp_index = self.all_indexs[index]
+        
+        track = self.list_of_tracks[tmp_index]
+        if self.random:
+            nl_idx = int(random.uniform(0, 3))
+            frame_idx = int(random.uniform(0, len(track["frames"])))
+        else:
+            nl_idx = 2
+            frame_idx = 0
+        text = track["nl"][nl_idx]
+        
+        video_path = os.path.join(self.data_cfg.CITYFLOW_PATH, self.list_of_uuids[tmp_index])
+        
+        info = get_worker_info()
+        video = cv2.VideoCapture(video_path)
+
+        if info is None:
+            rng = FrameRange(video, self.first, self.last)
+        else:
+            per = int(math.ceil((self.last - self.first) / float(info.num_workers)))
+            wid = info.id
+
+            first = self.first + wid * per
+            last = min(first + per, self.last)
+
+            rng = FrameRange(video, first, last)
+
+        if self.transform is not None:
+            fn = self.transform
+        else:
+            fn = lambda v: v  # noqa: E731
+
+        return TransformedRange(BatchedRange(rng, self.clip), fn), text, tmp_index
+
+
+class FrameRange:
+    def __init__(self, video, first, last):
+        assert first <= last
+
+        for i in range(first):
+            ret, _ = video.read()
+
+            if not ret:
+                raise RuntimeError("seeking to frame at index {} failed".format(i))
+
+        self.video = video
+        self.it = first
+        self.last = last
+
+    def __next__(self):
+        if self.it >= self.last or not self.video.isOpened():
+            raise StopIteration
+
+        ok, frame = self.video.read()
+
+        if not ok:
+            raise RuntimeError("decoding frame at index {} failed".format(self.it))
+
+        self.it += 1
+
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+class BatchedRange:
+    def __init__(self, rng, n):
+        self.rng = rng
+        self.n = n
+
+    def __next__(self):
+        ret = []
+
+        for i in range(self.n):
+            ret.append(next(self.rng))
+
+        return ret
+
+
+class TransformedRange:
+    def __init__(self, rng, fn):
+        self.rng = rng
+        self.fn = fn
+
+    def __next__(self):
+        return self.fn(next(self.rng))
