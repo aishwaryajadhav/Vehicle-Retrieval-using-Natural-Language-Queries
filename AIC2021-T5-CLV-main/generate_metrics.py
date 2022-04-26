61629ea39776b078b9b4f302cbe84a14ae0f582a
Ubuntu <ubuntu@ip-172-31-88-104.ec2.internal>
2022-04-26 03:51:16 +0000

    add scripts to generate results and metrics

---
+++ AIC2021-T5-CLV-main/generate_metrics.py
@@ -0,0 +1,73 @@
+import json
+import heapq
+from PIL import Image, ImageDraw
+import os
+import random
+import numpy as np
+import sys
+
+json_file = sys.argv[1]
+
+def recall(restuls, at=5):
+    c = 0
+    RR = 0
+    for k in results:
+        resultK = results[k]
+        for i in range(at):
+            if resultK[i] == k:
+                RR += 1
+                break
+        c += 1
+    return RR/c
+
+def MRR(results):
+    c = 0
+    RR = 0
+    for k in results:
+        resultK = results[k]
+        rank = 1
+        for i in range(len(resultK)):
+            if resultK[i] == k:
+                break
+            rank += 1
+        RR += 1/rank
+        c += 1
+    return RR/c
+
+def best(results, n=20):
+    q = []
+    heapq.heapify(q)
+
+    for k in results:
+        resultK = results[k]
+        rank = 1
+        for i in range(len(resultK)):
+            if resultK[i] == k:
+                break
+            rank += 1
+        heapq.heappush(q, (-rank, k))
+        if len(q) > n:
+            heapq.heappop(q)
+    return q
+
+def worst(results, n=20):
+    q = []
+    heapq.heapify(q)
+
+    for k in results:
+        resultK = results[k]
+        rank = 1
+        for i in range(len(resultK)):
+            if resultK[i] == k:
+                break
+            rank += 1
+        heapq.heappush(q, (rank, k))
+        if len(q) > n:
+            heapq.heappop(q)
+    return q
+
+
+results = json.load(open(json_file))
+print(MRR(results))
+print(recall(results, 5))
+print(recall(results, 10))
